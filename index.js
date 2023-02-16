4f2b26b2537da5651b83c0e9818291dc3efe17f8
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-16 22:36:32 +0530

    updated index.js

--- index.js
+++ index.js
@@ -27,3 +27,33 @@ let person={
 
 console.log(person.age);
 
+
+let e=3;
+let r=6;
+let t= e+r;
+
+console.log(t);
+
+let p= 10;
+
+console.log(++p);
+
+
+let q= 5;
+let w= 5;
+
+let ans1= (++q)*(--w);
+let ans2= (q++)*(--w);
+let ans3= (q++)*(w--);
+let ans4= (++q)*(w--);
+console.log(ans1);
+console.log(ans2);
+
+console.log(ans3);
+console.log(ans4);
+
+
+// 
+age =17;
+let status =(age>=18)?"you can vote":'you can not vote'
+ console.log(status);
