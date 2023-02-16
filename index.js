cf4815e923123c725b0b1e8c8acd5fb5fec6cdfa
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-16 21:12:10 +0530

    created index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+
+console.log('namaste JII aap kaise hai');
+let a =5;
+console.log(a);
+
+let name='rashu'
+console.log(name);
+const c="rashu"
+var sur='pandey';
+var sur='rashu pandey';
+
+console.log(sur);
+
+var naam='babbar'
+console.log(naam);
+
+naam=5;
+
+console.log(naam);
+
+// obejcts
+
+let person={
+    firstnaam:'rasshu',
+    age:19
+};
+
+console.log(person.age);
+
