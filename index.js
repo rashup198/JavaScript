1a235710ab3d76509b14936b1cad298c174089f8
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-18 11:19:13 +0530

    updated index.js

--- index.js
+++ index.js
@@ -196,5 +196,37 @@ else{
     console.log("You are a Kid");
 }
 
+// loops in js
+
+var arr=[1,2,3,4,5,6,7,7];
+console.log(arr);
+
+for(var i=0; i<arr.length; i++){
+    console.log(arr[i]);
+}
 
 
+arr.forEach(function(element) {
+    console.log(element);
+});
+
+// let j=4;
+// const ac=0;
+
+
+// console.log(ac);
+// console.log(j);
+let j=0;
+while(j<arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+
+do{
+    console.log(arr[j]);
+    j++;
+}
+while(j<arr.length);
+
+// breake and contionious statements
