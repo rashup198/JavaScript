847fb4fdfc93ab0bccf4938bd7230bb8c18d3b46
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-24 00:37:48 +0530

    updated  array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+let a =[1,2,3,34,213, false , "not present"]
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length);
+a[4]=45;
+a[2]=23;
+
+console.log(a);
+
