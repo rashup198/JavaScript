a1f21584af17e8e0054b507d54908320d9c6393b
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-24 01:02:14 +0530

    updated index.js

--- array.js
+++ array.js
@@ -14,3 +14,39 @@ a[2]=23;
 
 console.log(a);
 
+// methods in array
+// to string
+
+let b = a.toString()
+console.log(b);
+console.log(typeof b);
+
+// join
+
+let c= a.join("-")
+console.log(c, typeof c);
+
+//  pop
+
+let d=a.pop() // it will return the popped element
+
+console.log(d);
+
+
+// push
+
+let r= a.push(56)
+console.log(a);
+
+
+// shift
+
+let k= a.shift() //remove and element from start
+console.log(k);
+
+// unshift
+let j= a.unshift(78)
+console.log(j, a);
+
+
+
