8c54c7ece965d40f4fb03bcc1c5c5a06467f386e
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-02-24 02:02:23 +0530

    updated index.js

--- array.js
+++ array.js
@@ -48,5 +48,32 @@ console.log(k);
 let j= a.unshift(78)
 console.log(j, a);
 
+let number=[1,2,3,4,5,6,7,8,9]
+let number1=[11,22,33,44,55,66,77,88,99]
+
+console.log(number.length);
+delete number[0]
+console.log(number.length);
+
+let newArr = number.concat(number1)
+
+console.log(newArr);
+
+// sorting
+
+let arr=[32,1,2,445621,213,13,123,123123]
+
+let sortinf= arr.sort()
+
+console.log(arr);
+
+// reverse
+console.log(arr.reverse)
 
 
+// splice and slice
+
+let arr3=[ 23,34,45,56,67,78,89,90]
+arr3.splice(2,3,102,121,234,456,256) //first number is the starting address second address is how many element you want to remove and after that numbers are which you want to add
+
+console.log(arr3);
