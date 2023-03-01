7a3eafffec9b77b82a18b4931b7a0b14a505cb4e
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-01 16:59:39 +0530

    updated clouser.js

--- clouser.js
+++ clouser.js
@@ -3,6 +3,7 @@
 // clouser is the combination of fucntion and its lexical scope bundelled together 
 
 function outer(){
+    var a=10
     function inner(){
         console.log(a);
     }
@@ -11,3 +12,23 @@ function outer(){
 }
 
 outer()();
+
+// data hiding and encapsulation
+
+
+// lets say a variable has some data and we dont want a function to access it so there we use data hiding and data incapsulation
+
+function Counter(){
+var count=0;
+ this.incrementCounter = function (){
+     count++;
+     console.log(count);
+}
+this.decrementCounter = function (){
+    count--;
+    console.log(count);
+    }
+}
+
+var counter1= new Counter();
+counter1.incrementCounter();
