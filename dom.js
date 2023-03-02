f9d438d909dfadb4f517066606005171a9ee1ad1
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-02 10:41:21 +0530

    created dom.js

---
+++ dom.js
@@ -0,0 +1,9 @@
+const changeBodyRed =()=>{
+
+    document.body.firstElementChild.style.background = "red"
+}
+
+let b= document.body;
+
+console.log("first child of b is: ",  b.firstChild);
+console.log("firt element of child of b is : " , b.firstElementChild);
