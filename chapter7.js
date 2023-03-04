fc5498dcbe274c0f5951191bb5e0bd10e67071e2
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-04 11:51:48 +0530

    updated chapter7 practice set

--- chapter7.js
+++ chapter7.js
@@ -1,1 +1,10 @@
-document.getElementsByTagName("nav")
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
+
+document.getElementsByTagName("nav")[0].firstElementChild.style.color="green"
+
+document.getElementsByTagName("nav")[0].lastElementChild.style.color="green"
+
+
+Array.from(document.getElementsByTagName("nav")).forEach((Element)=>{
+    Element.style.background="cyan";
+})
