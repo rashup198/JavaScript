68ce0808450e0ba4360148700687b603254e7c7d
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-04 12:25:55 +0530

    updated exercise 2

--- excercise2.js
+++ excercise2.js
@@ -1,11 +1,34 @@
- let comp= [S,W,G]
 
 
- let inp = prompt("Enter your Input (S/W/G")
 
-let chances = 10;
+ let inp = prompt("Enter your Input (S/W/G)")
+ let comp= Math.floor(Math.random()*3);
+
+let comp_inp =["S","W","G"][comp]
 
 
-while(i<chances){
-    if((comp))
+const match =(comp_inp, inp)=>{
+    if(comp_inp==inp){
+        return "Nobody. Match is Tied"
+    }
+    else if(comp_inp=== "S" && inp==="W"){
+        return "Computer"
+    }
+    else if(comp_inp=== "G" && inp==="W"){
+        return "User"
+    }
+    else if(comp_inp=== "S" && inp==="G"){
+        return "User"
+    }
+    else if(comp_inp=== "G" && inp==="S"){
+        return "Computer"
+    }
+    else if(comp_inp=== "W" && inp==="G"){
+        return "User"
+    }
+    else if(comp_inp=== "W" && inp==="S"){
+        return "Computer"
+    }
 }
+let result = match(comp_inp, inp)
+document.write(`COMPUTER:${comp_inp} <br> User :${inp} <br> The winner is:${result.toLocaleUpperCase()}`);
