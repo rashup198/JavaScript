c94132239bc2a9e4e108458b01b904ea611da5e9
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-05 21:30:06 +0530

    updated set interval $ set time out

---
+++ interver_setTimeout/setinterval.js
@@ -0,0 +1,20 @@
+document.write("hello jiii")
+
+
+
+const sum= (a,b)=>{
+    console.log("yes i am running" + (a+b));
+    a+b}
+setTimeout(sum, 1000, 1,2)
+
+//  let a =setTimeout(function(){
+//     alert("i am inside of settimeout")
+// },5000)
+
+// let b = prompt("Do you want to see the  set timeout ?")
+
+// if("n"==b){
+
+//     clearTimeout(a)
+// }
+// console.log(a);
