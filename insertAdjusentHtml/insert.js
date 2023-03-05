1c9fa35c22e174e4285e28bd6c1712a07820428e
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-05 16:47:27 +0530

    updated insert Adjacent HTML

---
+++ insertAdjusentHtml/insert.js
@@ -0,0 +1,4 @@
+first.insertAdjacentHTML('beforebegin', '<div class="text"> beforebegin</div>');
+first.insertAdjacentHTML('beforeend', '<div class="text"> beforeend</div>');
+first.insertAdjacentHTML('afterbegin', '<div class="text"> afterbegin</div>');
+first.insertAdjacentHTML('afterend', '<div class="text"> afterend</div>');
