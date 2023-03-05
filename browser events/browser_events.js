3452ccfe1b7be482a5cd231098fe50920df468fb
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-05 22:38:09 +0530

    updated handling browser events

--- browser events/browser_events.js
+++ browser events/browser_events.js
@@ -4,3 +4,23 @@ a.onclick=()=>{
 
 b.innerHTML="hello jiii kaise hoo"
 }
+
+
+let x=function(){
+        alert('hello jiii 11')
+    
+    }
+
+btn.addEventListener('click', x)
+
+let y=function(){
+    alert('hello jiii 22')
+}
+btn.addEventListener('click', y)
+
+
+let c = prompt('what is your fav number?')
+
+if(c=="2"){
+    btn.removeEventListener('click', x)
+}
