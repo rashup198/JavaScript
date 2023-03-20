5de65f5fb11d8c3a6cc24f7866bb37c67a5cd5c3
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-20 22:36:48 +0530

    updated error handling.js

---
+++ error Handling/errorHandling.js
@@ -0,0 +1,26 @@
+ setTimeout(()=>{
+    console.log("Hacking Wifi.... Please Wait");
+ }, 1000);
+
+try{
+    console.log(rashu);
+}catch(error){
+    console.log(error);
+}
+
+
+ setTimeout(()=>{
+    console.log("Featching username and Password.... Please Wait");
+ }, 2000);
+
+
+ setTimeout(()=>{
+    console.log("hacking Rahul's facecbook ID .... Please Wait");
+ }, 3000);
+
+
+ setTimeout(()=>{
+    console.log("Fetched Username and Password of Rahul (+912341123213) ");
+ }, 4000);
+
+ 
