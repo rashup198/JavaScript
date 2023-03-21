db4fb6294fdf97a2567a2f736c167c097c5dbe8d
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-03-22 03:07:44 +0530

    updated finally clause.js

---
+++ error Handling/finallyClause.js
@@ -0,0 +1,20 @@
+const f = () => {
+
+
+    try {
+        let a = 0;
+       // console.log(program);
+        console.log("program run successfully");
+    } catch (err) {
+        console.log("this is an error");
+        console.log(p);
+    } finally {
+        console.log("i am a good boy");
+        //close the file
+        //exit the loop
+        //write to the log file;
+    }
+
+}
+
+f()
