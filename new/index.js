406f6ddd0eaf3113c63e9e2ff0e3bbb6999c280d
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-09 21:20:26 +0530

    updated

---
+++ new/index.js
@@ -0,0 +1,71 @@
+//  word vs keyword
+// console.log(a);
+// var a =12;
+
+// // loop - for , while
+
+// for (let index = 0; index <30; index++) {
+//     const element = "rashu";
+//     console.log(element);
+    
+// }
+
+// for(let i=25; i<50; i++){
+//     console.log(i);
+// }
+
+
+// while(a<20){
+//     a++;
+//     console.log(a);
+
+// }
+var b =0;
+while (b<10) {
+    
+    console.log(b, b**2);
+    b++
+}
+
+var b=105;
+while (b>7) {
+    b= b-7
+    console.log(b);
+}
+
+var b=10;
+while (b>1) {
+    b=b-1
+    console.log(b);  
+}
+
+console.log("using for loop");
+for (var i =10; i>0; i-- ){
+    console.log(i);
+}
+
+console.log("first 10 natural no.");
+var b =0;
+while(b<10){
+    b=b+1
+    console.log(b);
+}
+
+console.log("using  for loop");
+
+for (var i=0; i<=10; i++){
+    console.log(i);
+}
+
+console.log("first 10 event number");
+
+var b =0;
+
+
+while(b<20){
+    if(b%2==0){
+        console.log(b);
+        b++
+    }
+}
+
