90fe707f7204da6fee40c5a18c76547df5c50d11
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-08-17 23:45:32 +0530

    updated script.js

--- Dom/script.js
+++ Dom/script.js
@@ -31,3 +31,41 @@ a.addEventListener("click",function(){
     console.log("I am clicked");
 
 })
+
+var bulb =document.querySelector("#bulb")
+var btn =document.querySelector("button")
+
+var flag =0;
+
+btn.addEventListener("click",function(){
+    if(flag==0){
+       bulb.style.backgroundColor="yellow";
+       flag=1;
+    }
+    else{
+        bulb.style.backgroundColor="transparent"; 
+        flag=0;
+    }
+   
+})
+
+
+var h2Select= document.querySelector("h2")
+
+console.log(h2Select);
+
+var allSelctor = document.querySelectorAll("h2")
+console.log(allSelctor);
+
+
+allSelctor.forEach(function(e){
+    console.log(e  );
+
+})
+
+
+var box = document.querySelector("#box")
+
+box.innerHTML="<h1>Hello World</h1>";
+box.textContent="<h1>Hello World</h1>"
+
