5c4dee161b85a7c61000192634e65476bdc937cf
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-09-20 16:47:01 +0530

    updated

--- new/async/index.js
+++ new/async/index.js
@@ -34,7 +34,7 @@ const randcolor=()=>{
     const hex= "0123456789ABCDEF";
     let color= "#";
     for(let i=0;i<6;i++){
-        color += hex[Math.floor(Math.random()*16)];
+        color += hex[Math.floor(Math.ran dom()*16)];
     }
     return color;
 }
